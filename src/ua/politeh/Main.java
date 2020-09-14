70627c9350508e2a30dbb596e1017ac4eac95c41
valentin02 <Valentyn.Pereverten@o365.opu.ua>
2020-09-14 21:20:27 +0300

    lab2

--- src/ua/politeh/Main.java
+++ src/ua/politeh/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        Triangle tri = new Triangle(1.2, 2.2, 2.3);
     }
 }
--- src/ua/politeh/Triangle.java
+++ src/ua/politeh/Triangle.java
@@ -1,4 +1,18 @@
 package ua.politeh;
 
+import lombok.Getter;
+
 public class Triangle {
+
+    @Getter private double[] side;
+
+    public Triangle(double side0, double side1, double side2) {
+        this.side[0] = setSide(side0);
+        this.side[1] = setSide(side1);
+        this.side[2] = setSide(side2);
+    }
+
+    public double setSide(double side) {
+         return side > 0 ? side : 1;
+    }
 }
